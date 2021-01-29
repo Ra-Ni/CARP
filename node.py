d11c4bdcc3a864b92ab746dae7d474ab841bea2a
Rani <ranii.rafid@gmail.com>
2021-01-29 10:46:45 -0800

    changed list to set

--- node.py
+++ node.py
@@ -2,24 +2,25 @@ import uuid
 from collections import deque
 
 
-class NFA:
+class pNFA:
     def __init__(self, token=None):
-        self.start = str(uuid.uuid4())
-        self.final = str(uuid.uuid4())
+        self.initial_state = str(uuid.uuid4())
+        self.final_states = str(uuid.uuid4())
+        self.states = set()
         self.transitions = {}
         if token:
-            self.transitions[self.start] = {token: {self.final}}
+            self.transitions[self.initial_state] = {token: {self.final_states}}
 
     def print(self):
-        print("Start: {}\nFinal: {}\n".format(self.start, self.final))
+        print("Start: {}\nFinal: {}\n".format(self.initial_state, self.final_states))
         for k, v in self.transitions.items():
             print('{}: {}'.format(k, v))
         print()
 
     def clone(self):
-        backup = NFA()
-        uuid_map = {self.start: backup.start,
-                    self.final: backup.final}
+        backup = pNFA()
+        uuid_map = {self.initial_state: backup.initial_state,
+                    self.final_states: backup.final_states}
 
         for k, v in self.transitions.items():
             new_key = uuid_map.setdefault(k, str(uuid.uuid4()))
@@ -33,75 +34,74 @@ class NFA:
 
         return backup
 
-    def eclosure(self, entry):
-        path = deque([entry])
-        closure = []
-        while path:
-            entry = path.popleft()
-            if entry in self.transitions:
-                closure.append(entry)
-                inputs = self.transitions[entry]
-                if None in inputs:
-                    path.extend(inputs[None])
+    def normalize(self):
+        num_transitions = len(self.states)
+        transition_states = ['q{}'.format(i) for i in range(num_transitions)]
+        transitions_map = dict(zip(self.states, transition_states))
+        transitions_map[self.final_states] = 'q{}'.format(num_transitions)
 
-        return closure
+        for state, sub_dict in self.transitions.items():
+            for input, next_state in sub_dict.items():
+                buffer = list(next_state)
+                buffer = [transitions_map[buffer[i]] for i in range(len(buffer))]
+                self.transitions[state][input] = set(buffer)
 
-    def traverse(self, closure_set):
-        union_inputs = {}
-        for state in closure_set:
-            for k, v in self.transitions[state].items():
-                input = union_inputs.setdefault(k, set())
-                union_inputs[k] = input.union(v)
+        for key in list(self.transitions.keys()):
+            self.transitions[transitions_map[key]] = self.transitions.pop(key)
 
-        del union_inputs['']
-        for k, v in union_inputs.items():
-            union_inputs[k] = list(v)
-        return union_inputs
+        self.final_states = transitions_map[self.final_states]
+        self.initial_state = transitions_map[self.initial_state]
+        self.states = {transitions_map[i] for i in self.states}
+        final_state = 'q{}'.format(num_transitions)
+        self.states.add(final_state)
+        self.transitions[final_state] = {}
 
-    def finals(self):
 
-        return NotImplementedError
+
 
 
     @classmethod
     def concat(cls, x, y):
         new_NFA = cls()
-        new_NFA.transitions.update(x.transitions)
-        new_NFA.transitions.update(y.transitions)
-        x_final = new_NFA.transitions.setdefault(x.final, {})
-        y_final = new_NFA.transitions.setdefault(y.final, {})
-        table_start = new_NFA.transitions.setdefault(new_NFA.start, {})
-
-        x_final[''] = {y.start}
-        table_start[''] = {x.start}
-        y_final[''] = {new_NFA.final}
+        u, v = x.clone(), y.clone()
+        new_NFA.transitions.update(u.transitions)
+        new_NFA.transitions.update(v.transitions)
+        u_final = new_NFA.transitions.setdefault(u.final_states, {})
+        v_final = new_NFA.transitions.setdefault(v.final_states, {})
+        table_start = new_NFA.transitions.setdefault(new_NFA.initial_state, {})
+        u_final[''] = {v.initial_state}
+        table_start[''] = {u.initial_state}
+        v_final[''] = {new_NFA.final_states}
+        new_NFA.states = set(new_NFA.transitions.keys())
         return new_NFA
 
     @classmethod
     def union(cls, x, y):
         new_NFA = cls()
-        new_NFA.transitions.update(x.transitions)
-        new_NFA.transitions.update(y.transitions)
-        x_final = new_NFA.transitions.setdefault(x.final, {})
-        y_final = new_NFA.transitions.setdefault(y.final, {})
-        table_start = new_NFA.transitions.setdefault(new_NFA.start, {})
-
-        x_final[''] = {new_NFA.final}
-        table_start[''] = {x.start, y.start}
-        y_final[''] = {new_NFA.final}
+        u, v = x.clone(), y.clone()
+        new_NFA.transitions.update(u.transitions)
+        new_NFA.transitions.update(v.transitions)
+        u_final = new_NFA.transitions.setdefault(u.final_states, {})
+        v_final = new_NFA.transitions.setdefault(v.final_states, {})
+        table_start = new_NFA.transitions.setdefault(new_NFA.initial_state, {})
+        u_final[''] = {new_NFA.final_states}
+        table_start[''] = {u.initial_state, v.initial_state}
+        v_final[''] = {new_NFA.final_states}
+        new_NFA.states = set(new_NFA.transitions.keys())
         return new_NFA
 
     @classmethod
     def kleene_star(cls, x):
         new_NFA = cls()
-        new_NFA.transitions.update(x.transitions)
-        x_final = new_NFA.transitions.setdefault(x.final, {})
-        table_start = new_NFA.transitions.setdefault(new_NFA.start, {})
+        u = x.clone()
+        new_NFA.transitions.update(u.transitions)
+        x_final = new_NFA.transitions.setdefault(u.final_states, {})
+        table_start = new_NFA.transitions.setdefault(new_NFA.initial_state, {})
         x_final = x_final.setdefault('', set())
         table_start = table_start.setdefault('', set())
-
-        x_final.add(x.start)
-        x_final.add(new_NFA.final)
-        table_start.add(x.start)
-        table_start.add(new_NFA.final)
+        x_final.add(u.initial_state)
+        x_final.add(new_NFA.final_states)
+        table_start.add(u.initial_state)
+        table_start.add(new_NFA.final_states)
+        new_NFA.states = set(new_NFA.transitions.keys())
         return new_NFA
