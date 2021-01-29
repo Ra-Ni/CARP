2fc7be1da44ca653c225152bc9eed5f7aa6b0363
Rani <ranii.rafid@gmail.com>
2021-01-29 10:46:59 -0800

    added automata lib to convert from custom nfa to the lib's nfa

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from collections import deque
-
+from automata.fa.nfa import NFA
+from automata.fa.dfa import DFA
 from node import *
 
 
@@ -69,19 +70,31 @@ def shunting(text):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    table = NFA('x')
-    table2 = NFA('y')
-    tabl4 = table.clone()
-    table3 = NFA.union(table.clone(), table2.clone())
-    table5 = NFA.concat(table3.clone(), table.clone())
-
+    table = pNFA('x')
+    table2 = pNFA('y')
+    #(x|y)xy
+    table3 = pNFA.union(table, table2)
+    table5 = pNFA.concat(table3, table)
+    table5 = pNFA.concat(table5, table2)
     table5.print()
-    s = table5.eclosure(table5.start)
-    print(s)
-    d = table5.traverse(s)
-    print(d)
+    print(table5.states)
+    table5.normalize()
+    table5.print()
 
-
+    print(table5.transitions)
+    nfa = NFA(
+        states=table5.states,
+        input_symbols={'x', 'y'},
+        transitions=table5.transitions,
+        initial_state=table5.initial_state,
+        final_states={table5.final_states}
+    )
+    print(nfa.accepts_input(''))
+    print({table5.final_states})
+    print(nfa.validate())
+    dfa= DFA.from_nfa(nfa)
+    dfa = dfa.minify()
+    dfa.show_diagram('test.png')
     #table4 = NFA.union(table2, table3)
     #table5 = NFA.kleene_star(table4)
     # table6 = table.clone()
