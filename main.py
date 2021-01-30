00131196b76d0e8672ea313a52a347614732a946
Rani <ranii.rafid@gmail.com>
2021-01-29 22:26:04 -0800

    added tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 from collections import deque
+
+from automata.base.exceptions import RejectionException
 from automata.fa.nfa import NFA
 from automata.fa.dfa import DFA
 from fileparser import reader, parse
@@ -9,24 +11,23 @@ from node import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    table = pNFA('x')
-    table2 = pNFA('y')
-    #(x|y)xy
-    table3 = pNFA.union(table, table2)
-    table3.print()
-    table5 = pNFA.concat(table3, table)
-    table5.print()
-    table5 = pNFA.concat(table5, table2)
-
-    table5.print()
-
-    table5.normalize()
-    table5.print()
-
+    mode = 'id'
+    query = 'abc1'
 
     config = reader()
-    parse(config)
-    #table4 = NFA.union(table2, table3)
+    ref = parse(config)
+    dfa = ref[mode]
+
+    print('Accepts input \"{}\"? {}'.format(query, dfa.accepts_input(query)))
+    print('Accept states: {}'.format(str(dfa.final_states)))
+    try:
+        for answer in dfa.read_input_stepwise(query):
+            print(answer)
+
+    except RejectionException:
+        print("Rejected")
+
+        #table4 = NFA.union(table2, table3)
     #table5 = NFA.kleene_star(table4)
     # table6 = table.clone()
     # table.print()
