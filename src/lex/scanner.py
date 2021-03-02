587e3decbe3a6da023d8628f007db0e5aae99b0d
Rani <ranii.rafid@gmail.com>
2021-03-02 17:10:38 -0500

    scanner class refactored already to make more sense with the iterator

--- src/lex/scanner.py
+++ src/lex/scanner.py
@@ -5,23 +5,27 @@ from lex.token import token
 
 class scanner:
 
-    def __init__(self, config_file: str = None):
+    def __init__(self):
         self.tokens = None
         self.reserved = None
-        if config_file:
-            self.load(config_file)
+        self.text = None
+        self._opts = {}
 
-    def load(self, config_file: str):
-        with open(config_file, 'r') as fstream:
-            config = fstream.read()
-            config += "\nself.tokens = '|'.join('(?P<%s>%s)' % pair for pair in tokens)\nself.reserved = reserved"
-            exec(config)
+    def __inject__(self, code: str):
+        exec(code)
 
-    def read(self, data: str):
+    def open(self, file: str):
+        with open(file, 'r') as fstream:
+            text = fstream.read()
+        self.text = text
+
+    def options(self, **kwargs):
+        self._opts.update(kwargs)
+
+    def __iter__(self):
         line_num = 1
 
-        for tok in re.finditer(self.tokens, data):
-
+        for tok in re.finditer(self.tokens, self.text):
             kind = tok.lastgroup
             value = tok.group()
 
@@ -35,7 +39,23 @@ class scanner:
             elif kind == 'id' and value in self.reserved:
                 kind = str(value)
 
+            elif kind == 'inline_comment' or kind == 'block_comment' and self._opts['suppress_comments']:
+                line_num += value.count('\n')
+                continue
+
             yield token(str(kind), str(value), line_num)
 
             if kind == 'inline_comment' or kind == 'block_comment':
                 line_num += value.count('\n')
+
+    @classmethod
+    def load(cls, config: str, file: str = None):
+        reader = cls()
+        with open(config, 'r') as fstream:
+            code = fstream.read()
+            code += "\nself.tokens = '|'.join('(?P<%s>%s)' % pair for pair in tokens)\nself.reserved = reserved"
+            reader.__inject__(code)
+        if file:
+            reader.open(file)
+
+        return reader
